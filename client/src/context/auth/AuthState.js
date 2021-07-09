1d239d4173de3b86f31a7c463d85cf0fd0824acf
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-07-09 00:02:36 -0500

    idek

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from 'react';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import axios from 'axios';
+import setAuthToken from '../../utils/setAuthToken';
 
 import {
     REGISTER_SUCCESS,
@@ -27,13 +29,63 @@ const AuthState = props => {
 
     // Load User
 
+    const loadUser = async () => {
+        
+        if(localStorage.token) {
+
+            setAuthToken(localStorage.token);
+            
+        }
+
+        try {
+
+            const res = await axios.get('/api/auth');
+
+            dispatch({ type: USER_LOADED, payload: res.data });
+
+        } catch {
+
+            dispatch({ type: AUTH_ERROR });
+
+        }
+    };
+
     // Register User
+
+    const register = async formData => {
+        
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.post('/api/users', formData, config);
+
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            });
+
+            loadUser();
+
+        } catch (err) {
+
+            dispatch({
+                type: REGISTER_FAIL,
+                payload: err.response.data.msg
+            });
+
+        }
+    }
 
     // Login User
 
     // Logout
 
     // Clear Errors
+    const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
     
     return (
         <AuthContext.Provider 
@@ -42,7 +94,10 @@ const AuthState = props => {
                 isAuthenticated: state.isAuthenticated,
                 loading: state.loading,
                 user: state.user,
-                error: state.error
+                error: state.error,
+                register,
+                clearErrors,
+                loadUser
                 }}>
 
             { props.children }
