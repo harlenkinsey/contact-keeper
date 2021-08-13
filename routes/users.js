ed26d226a0198ed7b1f13a36e00782182123cfd8
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-08-13 17:30:09 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -51,7 +50,7 @@ router.post('/', [
             }
         }
 
-        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+        jwt.sign(payload, process.env['jwtSecret'], { expiresIn: 360000 }, (err, token) => {
             
             if(err) throw err;
 
